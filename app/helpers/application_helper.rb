fa3f28e00130028fb65a582f85a40966297665c7
hpathan <harunap@gmail.com>
2012-05-15 00:09:55 +0530

    small refactoring to remove default value from media dropdown and removed show template from video stats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-	def select_options_tag(name='',select_options={},options={})
-		selected = ''
-		unless options[:value].blank?
-			selected = options[:value]
-			options.delete(:value)
-		end
-		select_tag(name,options_for_select(select_options, selected), options)
-	end
 end
