502676175ba51788db08b64c6c63686a33129f78
Vasin Dmitriy <vasindima779@gmail.com>
2013-04-09 17:48:43 +0300

    Find error: initialize array with wrong data

--- app/controllers/refinery/reports_controller.rb
+++ app/controllers/refinery/reports_controller.rb
@@ -262,7 +262,7 @@ class Refinery::ReportsController < ApplicationController
     @report_day_array = ""
     @report_day_date = ""
 
-    @report_day_array_array = Array.new()
+    @report_day_array_array = Array.new(1, 'NA S!')
 
     Rails.logger.info '==='*20
     Rails.logger.info @report_day_array_array
