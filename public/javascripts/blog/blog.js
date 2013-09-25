ec5a24aa8e6a30f5259f9cfb4bc7575b04bd9a14
rskvirskiy <rskvirskiy@gmail.com>
2013-09-25 18:57:10 +0300

    #5: added print and comment button for post view

--- public/javascripts/blog/blog.js
+++ public/javascripts/blog/blog.js
@@ -3,4 +3,11 @@ $(document).ready(function () {
         var elem = $(this).children("ul");
         elem.toggle();
     });
+    $('.comment_button').live('click', function () {
+        $('#comments').toggle('slow');
+    });
+
+    $('.print_button').live('click', function () {
+        window.print();
+    });
 });
