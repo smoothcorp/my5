4034f67920c53eb305f1de45456a2861ae102a10
rskvirskiy <rskvirskiy@gmail.com>
2013-09-27 19:26:34 +0300

    #5: added functional for image and text alignment

--- public/javascripts/blog/blog.js
+++ public/javascripts/blog/blog.js
@@ -10,4 +10,17 @@ $(document).ready(function () {
     $('.print_button').live('click', function () {
         window.print();
     });
+    var image_right, image_left, image_center;
+    image_right = $('.image-align-right')
+    image_left = $('.image-align-left')
+    image_center = $('.text-align-center')
+    if (image_right)
+        image_right.parent().addClass('text-align-right');
+    if (image_left)
+        image_left.parent().addClass('text-align-left');
+    if (image_center)
+        $.each(image_center, function () {
+            if ($(this).is('img'))
+                $(this).parent().addClass('text-align-center');
+        });
 });
